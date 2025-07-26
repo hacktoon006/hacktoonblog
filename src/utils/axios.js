eb350baa959baac6514ab7a434ca34b5e8c36729
hacktoon006 <hacktoonworld@gmail.com>
2025-07-26 13:23:39 +0530

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL, // ✅ uses env variable
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
