175cf7f2109aaa4b13c7cf3f21a8246e85fa766c
hacktoon006 <hacktoonworld@gmail.com>
2025-07-26 12:06:31 +0530

    Update Login.jsx

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -23,7 +23,7 @@ const LoginPage = () => {
     setError("");
 
     try {
-      const res = await fetch(`http://localhost:5000/api/login`, {
+      const res = await fetch(`https://server-gqgt.onrender.com/api/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
