2b626627830763e1c66dcccc651b84c36f1185b6
hacktoon006 <hacktoonworld@gmail.com>
2025-07-26 12:11:16 +0530

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL, // ✅ uses env variable
+  baseURL: 'https://server-gqgt.onrender.com', // ✅ uses env variable
   withCredentials: true,
 });
 
