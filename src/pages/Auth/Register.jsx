ac2c978e88d05d18baaf52ba74dd0c1b78f496ff
hacktoon006 <hacktoonworld@gmail.com>
2025-07-26 12:06:48 +0530

    Update Register.jsx

--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -24,7 +24,7 @@ const RegisterPage = () => {
     setError("");
 
     try {
-      const res = await fetch(`http://localhost:5000/api/register`, {
+      const res = await fetch(`https://server-gqgt.onrender.com/api/register`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, email, password }),
